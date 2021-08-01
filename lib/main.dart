3f89d87214ef02b9082e5769ba06f8c8245f6b4c
parkminsoo99 <zzangorc99@naver.com>
2021-08-01 11:25:16 +0900

    flutter_git_test_7/29

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,42 @@
 import 'package:flutter/material.dart';
+import 'sub/firstPage.dart';
+import 'sub/secondPage.dart';
 
-final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+void main() => runApp(MyHomePage());
 
-void main() {
-  runApp(MyApp());
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
 
-  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter layout demo',
-        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter layout demo'),
-          ),
-          body: ListView(
-            children: [
-              Image.asset(
-              'image/lake.jpg',
-               width :600,
-               height: 240,
-               fit: BoxFit.cover,
-              ),
-              titleSection,
-              buttonSection,
-              textSection,
-            ],
-          ),
-        ));
+        home : Scaffold(
+            appBar: AppBar(
+              title: Text('TabBar Example'),
+            ),
+            body: TabBarView(
+              children: <Widget>[FirstApp(), SecondApp()],
+              controller: controller,
+            ),
+            bottomNavigationBar: TabBar(tabs: <Tab>[
+              Tab(icon: Icon(Icons.looks_one, color: Colors.blue),),
+              Tab(icon: Icon(Icons.looks_two, color: Colors.blue),),
+            ], controller: controller,)
+        ),
+    );
   }
-}
 
-Widget textSection = const Padding(
-  padding: EdgeInsets.all(32),
-  child: Text(
-    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-    'Alps. Situated 1,578 meters above sea level, it is one of the '
-    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-    'half-hour walk through pastures and pine forest, leads you to the '
-    'lake, which warms to 20 degrees Celsius in the summer. Activities '
-    'enjoyed here include rowing, and riding the summer toboggan run.',
-    softWrap: true,
-  ),
-);
-
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Kandersteg, switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('41'),
-    ],
-  ),
-);
-
-Column _buildButtonColumn(Color color, IconData icon, String label) {
-  return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ]);
-}
-
-Color color = Colors.blue;
-
-Widget buttonSection =
-    Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-  _buildButtonColumn(color, Icons.call, 'CALL'),
-  _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-  _buildButtonColumn(color, Icons.share, 'SHARE'),
-]);
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('Hello, World!', style: Theme.of(context).textTheme.headline4);
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
